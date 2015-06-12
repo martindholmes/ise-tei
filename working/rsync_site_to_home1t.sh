9ab33da84647f456288fa820f60289057d41c52b
martindholmes <mholmes@uvic.ca>
2015-06-12 09:57:15 -0700

    Removing local file from repo.

--- working/rsync_site_to_home1t.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
-
-echo "Uploading files from $DIR to server"
-
-rsync --verbose --progress --stats --recursive --times  --exclude ".svn/" -e ssh "$DIR/../dist/" hcmc@nfs.hcmc.uvic.ca:/home1t/hcmc/www/workshops/ise/
-
-echo "Done"
